1d0739b0fea8d21ca12504317cb781f68e6a01d4
Nicholas Devenish <ndevenish@gmail.com>
2022-05-11 10:46:42 +0100

    Another name fix

--- recipes/crystfel/run_test.sh
+++ recipes/crystfel/run_test.sh
@@ -38,15 +38,15 @@ _build/tests/prof2d_check
 _build/tests/ev_enum1 crystfel/tests/ev_enum1.h5 crystfel/tests/ev_enum1.geom
 _build/tests/ev_enum2 crystfel/tests/ev_enum2.h5 crystfel/tests/ev_enum2.geom
 _build/tests/ev_enum3 crystfel/tests/ev_enum3.h5 crystfel/tests/ev_enum3.geom
-_build/tests/wavelength_geom1 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom1.geom 1e-10
-_build/tests/wavelength_geom2 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom2.geom 1.3776e-10
-_build/tests/wavelength_geom3 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom3.geom 1.3776e-10
-_build/tests/wavelength_geom4 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom4.geom 1.9687e-12
-_build/tests/wavelength_geom5 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom5.geom 1.9687e-12
-_build/tests/wavelength_geom6 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom6.geom 1.3776e-10
-_build/tests/wavelength_geom7 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom7.geom 1.3776e-10
-_build/tests/wavelength_geom8 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom8.geom 1.9687e-12
-_build/tests/wavelength_geom9 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom9.geom 1.3776e-10
-_build/tests/wavelength_geom10 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom10.geom 1.3776e-10
-_build/tests/wavelength_geom11 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom11.geom 1.125e-10
-_build/tests/wavelength_geom12 crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom12.geom 1.125e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom1.geom 1e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom2.geom 1.3776e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom3.geom 1.3776e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom4.geom 1.9687e-12
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom5.geom 1.9687e-12
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom6.geom 1.3776e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom7.geom 1.3776e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom8.geom 1.9687e-12
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom9.geom 1.3776e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom10.geom 1.3776e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom11.geom 1.125e-10
+_build/tests/wavelength_geom crystfel/tests/wavelength_geom.h5 crystfel/tests/wavelength_geom12.geom 1.125e-10
